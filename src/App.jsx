66063a2f55232ed6b139962c10766b263b82e4ce
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-06-22 18:23:24 -0700

    set up front end react files and folders

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './App.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* <Route path="/" exact component={ } /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
