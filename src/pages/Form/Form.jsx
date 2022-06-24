f7659dd181c6dae154f652a267fac1f7cd729302
Tara Tohidi <tara.tohidi@gmail.com>
2022-06-23 22:36:17 -0700

    Style form

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -3,10 +3,10 @@ import "./Form.scss";
 function Form() {
   return (
     <>
-      <form id="form" method="POST">
-        <div className="form__section--box">
-          <div className="form__section">
-            <h3 className="form__subheader">Form</h3>
+      <form id="form" method="POST" className="form">
+        <h2 className="form__subheader">Form</h2>
+        <div className="form__section">
+          <div className="form__section--box">
             <label htmlFor="name" className="form__label">
               Name
             </label>
@@ -18,6 +18,8 @@ function Form() {
               placeholder="John Smith"
               required
             ></input>
+          </div>
+          <div className="form__section--box">
             <label htmlFor="email" className="form__label">
               Email
             </label>
@@ -29,6 +31,21 @@ function Form() {
               placeholder="John.Smith@form.com"
               required
             ></input>
+          </div>
+          <div className="form__section--box">
+            <label htmlFor="phone" className="form__label">
+              phone
+            </label>
+            <input
+              className="form__input"
+              id="phone"
+              name="phone"
+              type="tel"
+              placeholder="666-666-666"
+              required
+            ></input>
+          </div>
+          <div className="form__section--box">
             <label htmlFor="review" className="form__label">
               Add new review
             </label>
@@ -40,6 +57,8 @@ function Form() {
               placeholder="written review"
               required
             ></textarea>
+          </div>
+          <div className="form__section--box">
             <label className="form__label">How much</label>
             <select
               className="form__input"
@@ -48,28 +67,14 @@ function Form() {
               required
             >
               <option>Please Select</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
             </select>
           </div>
         </div>
-
-        <div className="add-inventory__button--box">
-          <button
-            className="add-inventory__cancel"
-            onClick={this.props.history.goBack}
-          >
-            Cancel
-          </button>
-          <button
-            className="add-inventory__button"
-            type="submit"
-            disabled={
-              (this.state.warehouse && this.state.category) === ""
-                ? true
-                : false
-            }
-          >
-            + Add Item
-          </button>
+        <div className="form__button--box">
+          <button className="form__button">submit</button>
         </div>
       </form>
     </>
